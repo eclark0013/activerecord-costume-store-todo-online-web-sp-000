2eb505351c5728ffd0bb342f20ffd7ef97ed5875
Eric Clark <eclark0013@gmail.com>
2019-08-22 14:36:40 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -14,8 +14,7 @@ class CreateCostumes < ActiveRecord::Migration[5.2]
       t.string :price
       t.string :size
       t.string :image_url
-      t.datetime :created_at
-      t.datetime :updated_at
+      t.timestamps
     end
   end
 end
