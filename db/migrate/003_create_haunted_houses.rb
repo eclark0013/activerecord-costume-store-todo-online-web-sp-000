4e0d398c4923ceb930f9ef025a762559c96f405b
Eric Clark <eclark0013@gmail.com>
2019-08-22 14:49:05 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -2,6 +2,14 @@
 class HauntedHouse < ActiveRecord::Migration[4.2]
   def change
     create_table :haunted_houses do |t|
+      name
+location
+theme
+price
+family_friendly
+opening_date
+closing_date
+long_description
     end
   end
 end
